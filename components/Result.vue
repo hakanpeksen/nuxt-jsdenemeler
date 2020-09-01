83dbbc4b1ea94e69b294243f531aaa896996695f
hakan <32062772+hakanpeksen@users.noreply.github.com>
2020-09-01 13:59:16 +0300

    counter-store sayfasında  buton ile  tıklanınca  vuex dolduruldu.

---
+++ components/Result.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>Sayaç:{{ counter }}</div>
+    <!-- <div>Tıklama:{{ stringCounter }}</div> -->
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter
+      // return this.$store.getters.getDoubleCounter
+      // return this.$store.getters.getCounter
+    },
+    stringCounter() {
+      return this.$store.getters.stringCounter
+    },
+  },
+}
+</script>
+
+<style scoped></style>
